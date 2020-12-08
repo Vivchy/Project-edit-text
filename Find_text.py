9b45c615bc7a3e3023d8429e0f86a6b069090e89
Vivchy <vivchy79@gmail.com>
2020-12-08 11:40:23 +0400

    simple csv

--- Find_text.py
+++ Find_text.py
@@ -8,7 +8,29 @@
 # диалоговое окно для поиска конкретного слова и просмотр предложения, где оно встречается.
 
 import re
+import csv
 
+games = [
+    ['1', 'Igor', '12','56'],
+    ['2', 'urii', '32', '25'],
+    ['3', 'Leonid', '12', '88'],
+    ['4', 'Dasha','16', '87']
+]
+with open('games', 'wt') as fout:
+    csvout = csv.writer(fout)
+    csvout.writerows(games)
+
+with open('games', 'rt') as fin:
+    cin = csv.DictReader(fin, fieldnames=['id', 'Name', 'age', 'score'])
+    games2 = [row for row in cin]
+    print(games2)
+    
+with open('newgame.csv', 'wt') as g:
+    cin = csv.DictWriter(g, ['id', 'Name', 'age', 'score'])
+    cin.writeheader()
+    cin.writerows(games2)
+
+print(c)
 def find_word(word, text):
     """
     получает слово и выводит колличество данных совпадений в тексте
